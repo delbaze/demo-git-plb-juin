e1b7d9d469270139e209f85ad22e9a098c62d89b
delbaze <56109313+delbaze@users.noreply.github.com>
2025-06-11 10:19:01 +0200

    correction

--- resolvers/author.resolver.ts
+++ resolvers/author.resolver.ts
@@ -3,20 +3,23 @@ let authors = [
     id: "A1",
     name: "Kate Chopin",
     nationality: "Américain",
-    birthYear: 1850
+    birthYear: 1850,
+    novels: ["The Awakening"], // Champ pour Novelist
   },
   {
     id: "A2",
     name: "Paul Auster",
     nationality: "Américain",
-    birthYear: 1947
+    birthYear: 1947,
+    novels: ["City of Glass"], // Champ pour Novelist*
   },
   {
     id: "A3",
     name: "Super mangaka",
     nationality: "Japonais",
-    birthYear: 1980
-  }
+    birthYear: 1980,
+    mangas: ["Mon Manga"], // Champ pour Mangaka
+  },
 ];
 
 export default {
@@ -28,7 +31,27 @@ export default {
         throw new Error("L'auteur n'existe pas");
       }
       return author;
-    }
+    },
+    findNovelistById: (_: any, args: any) => {
+      const author = authors.find((a) => a.id === args.id);
+      if (!author) {
+        throw new Error("L'auteur n'existe pas");
+      }
+      if (!author.novels) {
+        throw new Error("L'auteur n'est pas un romancier");
+      }
+      return author;
+    },
+    findMangakaById: (_: any, args: any) => {
+      const author = authors.find((a) => a.id === args.id);
+      if (!author) {
+        throw new Error("L'auteur n'existe pas");
+      }
+      if (!author.mangas) {
+        throw new Error("L'auteur n'est pas un mangaka");
+      }
+      return author;
+    },
   },
   Mutation: {
     addAuthor: (_: any, args: any) => {
@@ -36,7 +59,10 @@ export default {
         id: `A${Date.now()}`, // Génération simple d'un ID
         name: args.name,
         nationality: args.nationality,
-        birthYear: args.birthYear
+        birthYear: args.birthYear,
+        ...(args.isMangaka
+          ? { mangas: ["Nouveau Manga"] }
+          : { novels: ["Nouveau Roman"] }),
       };
       authors.push(newAuthor);
       return newAuthor;
@@ -50,7 +76,10 @@ export default {
         ...authors[authorIndex],
         name: args.name || authors[authorIndex].name, // Garde l'existant si non fourni
         nationality: args.nationality || authors[authorIndex].nationality,
-        birthYear: args.birthYear !== undefined ? args.birthYear : authors[authorIndex].birthYear
+        birthYear:
+          args.birthYear !== undefined
+            ? args.birthYear
+            : authors[authorIndex].birthYear,
       };
       authors[authorIndex] = updatedAuthor;
       return updatedAuthor;
@@ -62,8 +91,19 @@ export default {
       }
       authors = authors.filter((a) => a.id !== args.id);
       return {
-        message: "L'auteur a bien été supprimé"
+        message: "L'auteur a bien été supprimé",
       };
-    }
-  }
+    },
+  },
+  AnyAuthor: {
+    __resolveType(obj: any) {
+      if (obj.novels) {
+        return "Novelist";
+      }
+      if (obj.mangas) {
+        return "Mangaka";
+      }
+      throw new Error("Impossible de résoudre le type pour cet auteur");
+    },
+  },
 };
--- resolvers/book.resolver.ts
+++ resolvers/book.resolver.ts
@@ -3,16 +3,28 @@ let books = [
     id: 1,
     title: "The Awakening",
     author: "Kate Chopin",
+    demo: {
+      name: "toto",
+      age: 12,
+    },
   },
   {
     id: 2,
     title: "City of Glass",
     author: "Paul Auster",
+    demo: {
+      name: "tata",
+      age: 12,
+    },
   },
   {
     title: "Mon Manga",
-    author: "Super mangaka"
-  }
+    author: "Super mangaka",
+    demo: {
+      name: "titi",
+      age: 12,
+    },
+  },
 ];
 
 export default {
@@ -42,11 +54,20 @@ export default {
   },
   Book: {
     __resolveType(obj: any) {
-        if (obj.id){
-            return 'TextBook';
-        }
+      //   console.log("Obj", obj);
+      if (obj.id) {
+        return "TextBook";
+      }
 
-        return 'MangaBook';
-    }
-  }
+      return "MangaBook";
+    },
+  },
+  TextBook: {
+    demo: (parent: any, args: any) => {
+      console.log("PARENT", parent);
+      console.log("ARGS", args);
+
+      return { name: (parent.demo.name as string).toUpperCase(), age: parent.demo.age };
+    },
+  },
 };
